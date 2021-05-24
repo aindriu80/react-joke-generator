23d6bbedfa466fd92bba3d014799e01f9a3e8e7e
aindriu80 <aindriu80@gmail.com>
2021-05-24 15:58:35 +0100

    Creating the logic in React using hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from 'react'
+
+const API_URL = `https://api.icndb.com/jokes/random`
 function App() {
+  const [joke, setJoke] = useState('')
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setJoke(data.value.joke))
+  }
+
+  useEffect(() => {
+    generateJoke()
+  }, [])
+
   return (
     <div className="box">
       <h3>Chuck Norris Jokes Generator</h3>
-      <p>Here goes the joke</p>
-      <button>Get a new joke 🤣</button>
+      <p dangerouslySetInnerHTML={{ __html: joke }}></p>
+      <button onClick={generateJoke}>
+        Get a new joke
+        <span role="img" aria-label="laugh emoji">
+          {' '}
+          🤣
+        </span>
+      </button>
     </div>
   )
 }
